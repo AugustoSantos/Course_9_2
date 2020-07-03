2cfebecb7dc6e8db6cb248314640fd80db877693
Augusto Santos <augustosantoscordeiro@gmail.com>
2020-07-03 17:27:06 -0300

    Projeto inicializado, tendo sido criadas as classes HourContract e Department

---
+++ Course_9_2/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Course_9_2.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
